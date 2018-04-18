b620aadb63ba573801991fa4afbf84a0ab3e24e3
Mout Pessemier <mout.pessemier@student.hogent.be>
2018-04-18 22:49:19 +0200

    ColorPicker W.I.P, style changes  v2.7

--- JS/colorPicker.js
+++ JS/colorPicker.js
@@ -1,8 +1,17 @@
-var swatches = document.querySelectorAll('.swatches span');
-var root = document.querySelector(':root');
-swatches.forEach((swatch) => {
-    swatch.addEventListener('click', (e) => {
-        root.style.setProperty('--main-color', e.target.style.background);
-        root.style.setProperty('--accent-color', e.target.style.color);
-    })
-});
+const colors = document.getElementsByClassName('colorPicker');
+const colorBtnGreen = document.getElementsByClassName('color1');
+const colorBtnYellow = document.getElementsByClassName('color2');
+
+function init() {
+  let color;
+  if (colorBtnGreen.onclick) {
+    color = colors[0].style.backgroundColor;
+  }
+  if (colorBtnYellow.onclick) {
+    color = colors[1].style.backgroundColor;
+  }
+
+  //loop over mijn css file en stel elke keer de kleur in op color
+}
+
+window.onload = init;
