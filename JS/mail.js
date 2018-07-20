6b2ed90e4e36c13cda2b49ef52c5c761e97f3158
Mout Pessemier <mout.pessemier@student.hogent.be>
2018-07-20 20:17:19 +0200

    v5.3.3 online test eport mail service

--- JS/mail.js
+++ JS/mail.js
@@ -1,34 +1,3 @@
-// function init() {
-//     const feedback = document.getElementById('feedback');
-
-//     document.getElementById('mail').addEventListener('submit', e => {
-//         let name = document.getElementById('name');
-//         let email = document.getElementById('email');
-//         let message = document.getElementById('message');
-
-//         if (!name.value || !email.value || !message.value) {
-//             feedback.style.backgroundColor = 'rgba(170,4,34,0.7)';
-//             feedback.style.visibility = 'visible';
-//             feedback.innerHTML = "Check your entries!"
-//             setTimeout(() => {
-//                 feedback.style.visibility = 'hidden';
-//             }, 3000);
-//             e.preventDefault();
-//         } else {
-
-//             feedback.style.backgroundColor = 'rgba(0,170,16,0.7)';
-//             feedback.style.visibility = 'visible';
-//             feedback.innerHTML = "Message sent!"
-//             name.innerHTML = "";
-//             email.innerHTML = "";
-//             message.innerHTML = "";
-//             setTimeout(() => {
-//                 feedback.style.visibility = 'hidden';
-//             }, 3000);
-//         }
-//     })
-// }
-
 function init() {
     let good;
     let bad;
@@ -41,28 +10,28 @@ function init() {
     }
 
     $('#mail').submit((e) => {
-        let name = document.getElementById('name');
-        let email = document.getElementById('email');
-        let message = document.getElementById('message');
+            let name = document.getElementById('name');
+            let email = document.getElementById('email');
+            let messageM = document.getElementById('message');
 
-        console.log(name.value); // niets?
-        console.log(email.value); // niets?
-        console.log(message.value); // print content
+            console.log(name.value);
+            console.log(email.value);
+            console.log(message.value);
 
-        if (!name.value || !email.value || !message.value) {
-            feedback.style.backgroundColor = 'rgba(170,4,34,0.7)';
-            feedback.style.visibility = 'visible';
-            feedback.innerHTML = bad;
-            setTimeout(() => {
-                feedback.style.visibility = 'hidden';
-            }, 3000);
+            // if (!name.value || !email.value || !messageM.value) {
+            //     feedback.style.backgroundColor = 'rgba(170,4,34,0.7)';
+            //     feedback.style.visibility = 'visible';
+            //     feedback.innerHTML = bad;
+            //     setTimeout(() => {
+            //         feedback.style.visibility = 'hidden';
+            //     }, 3000);
 
-        } else {
+            // } else {
             $.ajax({
-                url: "https://formspree.io/moutpessemier@hotmail.com",
-                method: "POST",
+                url: "https://formspree.io/mail",
+                type: "POST",
                 data: {
-                    message: $(this).serialize()
+                    message: `${name.value}\n${email.value}\n\n${messageM.value}`
                 },
                 dataType: "json"
             });
@@ -73,11 +42,13 @@ function init() {
             setTimeout(() => {
                 feedback.style.visibility = 'hidden';
             }, 3000);
-            $(this).get(0).reset();
+            name.value = "";
+            email.value = "";
+            message.value = "";
+
         }
-    });
+        //}
+    );
 }
 
 window.onload = () => init();
-
-document.la
