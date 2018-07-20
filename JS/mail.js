2b043fb4cff22f22347513a31bf32ff6996520fd
Mout Pessemier <mout.pessemier@student.hogent.be>
2018-07-20 20:34:40 +0200

    v5.3.4 updated script

--- JS/mail.js
+++ JS/mail.js
@@ -1,54 +1,56 @@
 function init() {
-    let good;
-    let bad;
-    if (document.documentElement.lang === "en") {
-        good = "Message sent!";
-        bad = "Check your entries!";
-    } else {
-        good = "Mail verzonden!";
-        bad = "Vul alles in!";
+  let good;
+  let bad;
+  if (document.documentElement.lang === 'en') {
+    good = 'Message sent!';
+    bad = 'Check your entries!';
+  } else {
+    good = 'Mail verzonden!';
+    bad = 'Vul alles in!';
+  }
+
+  $('#mail').submit(
+    e => {
+      let name = document.getElementById('name');
+      let email = document.getElementById('email');
+      let messageM = document.getElementById('message');
+
+      console.log(name.value);
+      console.log(email.value);
+      console.log(message.value);
+
+      // if (!name.value || !email.value || !messageM.value) {
+      //     feedback.style.backgroundColor = 'rgba(170,4,34,0.7)';
+      //     feedback.style.visibility = 'visible';
+      //     feedback.innerHTML = bad;
+      //     setTimeout(() => {
+      //         feedback.style.visibility = 'hidden';
+      //     }, 3000);
+
+      // } else {
+      $.ajax({
+        url: 'https://formspree.io/mail',
+        type: 'POST',
+        data: {
+          name: name.value,
+          email: email.value,
+          message: messageM.value
+        },
+        dataType: 'json'
+      });
+      e.preventDefault();
+      feedback.style.backgroundColor = 'rgba(0,170,16,0.7)';
+      feedback.style.visibility = 'visible';
+      feedback.innerHTML = good;
+      setTimeout(() => {
+        feedback.style.visibility = 'hidden';
+      }, 3000);
+      name.value = '';
+      email.value = '';
+      message.value = '';
     }
-
-    $('#mail').submit((e) => {
-            let name = document.getElementById('name');
-            let email = document.getElementById('email');
-            let messageM = document.getElementById('message');
-
-            console.log(name.value);
-            console.log(email.value);
-            console.log(message.value);
-
-            // if (!name.value || !email.value || !messageM.value) {
-            //     feedback.style.backgroundColor = 'rgba(170,4,34,0.7)';
-            //     feedback.style.visibility = 'visible';
-            //     feedback.innerHTML = bad;
-            //     setTimeout(() => {
-            //         feedback.style.visibility = 'hidden';
-            //     }, 3000);
-
-            // } else {
-            $.ajax({
-                url: "https://formspree.io/mail",
-                type: "POST",
-                data: {
-                    message: `${name.value}\n${email.value}\n\n${messageM.value}`
-                },
-                dataType: "json"
-            });
-            e.preventDefault();
-            feedback.style.backgroundColor = 'rgba(0,170,16,0.7)';
-            feedback.style.visibility = 'visible';
-            feedback.innerHTML = good;
-            setTimeout(() => {
-                feedback.style.visibility = 'hidden';
-            }, 3000);
-            name.value = "";
-            email.value = "";
-            message.value = "";
-
-        }
-        //}
-    );
+    //}
+  );
 }
 
 window.onload = () => init();
